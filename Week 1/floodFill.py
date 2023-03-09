d8a83685ca8ded4825930e47e4ce870bb8ad3b6f
Pablo Navarro Zepeda <panaze@outlook.com>
2023-03-08 20:42:03 -0600

    Added solution for flood fill problem with DFS

--- Week 1/floodFill.py
+++ Week 1/floodFill.py
@@ -1,15 +1,34 @@
-def floodFill( image, sr, sc, color):
-        if image[sr][sc] == color:
-          return image
-        else:
-            image[sr][sc] = color
-            
-            image[sr-1][sc-1] = color
-            image[sr-1][sc] = color
+def floodFill(image, sr, sc, color):
+     x = len(image)
+     y = len(image[0])
+    
+     newColor = color
+     originalColor = image[sr][sc]
 
-            image[sr][sc-1] = color
-            image[sr+1][sc-1] = color
-        return image
+     def dfs(i,j):
+        #BASE CASE
+        # If index is out of boundaries 
+        if i < 0 or i >= x or j < 0 or j >= y:
+            return
+         #If element is already painted or is not equal to the color of the center
+        if  image[i][j] == newColor or image[i][j] != originalColor:
+            return
+
+        else:
+            #Flood fill the element
+            image[i][j] = newColor
+
+            #Call recursevely the 4 directionally connected elements (up,down,    left and right)
+            dfs(i+1,j)
+            dfs(i-1,j)
+            dfs(i,j+1)
+            dfs(i,j-1)
+    
+     if image[sr][sc] != newColor:
+         dfs(sr,sc)
+        
+     return image
+
             
           
 
