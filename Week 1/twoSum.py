290fd7eb1fcb5516b7cff7e2c51ed02d5f867296
Pablo Navarro Zepeda <panaze@outlook.com>
2023-03-20 18:45:55 -0600

    Solved in O(n)

--- Week 1/twoSum.py
+++ Week 1/twoSum.py
@@ -1,11 +1,15 @@
 def twoSum(nums, target):
+    d = {}
     for i in range(len(nums)):
-      for j in range(i+1,len(nums)):
-        if nums[i] + nums[j] == target:
-          return [i,j]
+      r = target - nums[i]
+      if r in d: 
+        return [d[r], i]
+      d[nums[i]] = i
+		
 
 def main():
-    nums = [3,2,4]
-    print(twoSum(nums,6))
+    nums = [2,7,11,15]
+    target = 9
+    print(twoSum(nums, target))
 
 main()
